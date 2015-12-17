8ee2c59c616817c83e15c4ef8c73d7da889a17c5
kc <kc04bc@gmx.com>
2015-12-17 23:22:39 +0100

    Authorisation code added

--- main.hs
+++ main.hs
@@ -7,16 +7,19 @@
 {-# LANGUAGE QuasiQuotes                #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
-import           Control.Monad.Logger (runNoLoggingT)
-import           Data.Text                (Text, append, pack)
-import           Data.Typeable            (Typeable)
+import           Control.Monad.Logger       (runNoLoggingT)
+import           Control.Monad              (liftM)
+import           Network.HTTP.Client.Conduit (Manager, newManager)
+import           Data.Text                  (Text, append, pack)
+import           Data.Typeable              (Typeable)
 import           Database.Persist.Sqlite
-import           Database.Persist         (persistUniqueKeys)
-import           Text.Hamlet              (shamlet)
-import           Text.Shakespeare.Text    (stext)
+import           Database.Persist           (persistUniqueKeys)
+import           Text.Hamlet                (shamlet)
+import           Text.Shakespeare.Text      (stext)
 import           Yesod
 import           Yesod.Auth
-import qualified Yesod.Auth.HashDB        as HDB
+import qualified Yesod.Auth.Message         as Msg
+import qualified Yesod.Auth.HashDB          as HDB
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 User
@@ -26,16 +29,24 @@ User
     deriving Typeable
 |]
 
-
 mkYesod "App" [parseRoutes|
-    /           HomeR       GET
+    /           HomeR       GET     POST
     /auth       AuthR       Auth getAuth
     /register   RegisterR   POST -- better to be under AuthR
 |]
 
-data App = App SqlBackend
+adminName :: Text
+adminName = "admin"
+
+data App = App
+    { sqlBackEnd  :: SqlBackend
+    , httpManager :: Manager
+    }
 
 instance Yesod App where
+    authRoute _ = Just $ AuthR LoginR
+    isAuthorized HomeR True = isAdmin -- only admin can POST in HomeR
+    isAuthorized _ _ = return Authorized
 
 instance RenderMessage App FormMessage where
     renderMessage _ _ = defaultFormMessage
@@ -43,7 +54,7 @@ instance RenderMessage App FormMessage where
 instance YesodPersist App where
     type YesodPersistBackend App = SqlBackend
     runDB f = do
-        App conn <- getYesod
+        App conn _ <- getYesod
         runSqlConn f conn
 
 instance YesodAuthPersist App where
@@ -55,48 +66,42 @@ instance YesodAuth App where
     logoutDest _ = HomeR
     authPlugins _ = [ HDB.authHashDB (Just . UniqueUser) ]
     getAuthId creds = HDB.getAuthIdHashDB AuthR (Just . UniqueUser) creds
-    authHttpManager = error "No need an HTTP manager"
+    authHttpManager = httpManager
 
 instance HDB.HashDBUser User where
     userPasswordHash = Just . userPassword
     setPasswordHash h u = u { userPassword = h }
 
-succHtml :: User -> Handler Html
-succHtml user = do
+registerSucc :: User -> Handler ()
+registerSucc user = do
     succ <- addUserToDB user
     if succ
-        then defaultLayout
-                [whamlet|
-                    <p>
-                        Success!, now you can
-                        <a href=@{AuthR LoginR}>login.
-                |]
-        else defaultLayout
-                [whamlet|
-                    <p>
-                        User name already exist!, please try
-                        <a href=@{RegisterR}>again.
-                |]
+        then setMessage "Success!, please login." >> redirect (AuthR LoginR)
+        else setMessage "User name already exist, please try again." >> redirect HomeR
 
-postRegisterR :: Handler Html
+registerFail :: Handler ()
+registerFail = setMessage "Critical Error!" >> redirect HomeR
+
+postRegisterR :: Handler ()
 postRegisterR = do
     ((result, widget), enctype) <- runFormPost registerForm
     case result of
-        FormSuccess user -> succHtml user
-        _ -> defaultLayout
-                [whamlet|
-                    <p>Error, please try again.
-                    ^{registerWidget widget enctype}
-                |]
+        FormSuccess user -> registerSucc user
+        _ -> registerFail
 
 getHomeR :: Handler Html
 getHomeR = do
-    maid <- maybeAuthId
+    ma <- maybeAuth
+    let maUser = liftM (userName . entityVal) ma
     (widget, enctype) <- generateFormPost registerForm
     defaultLayout
         [whamlet|
-            <p>Your current auth ID: #{show maid}
-            $maybe _ <- maid
+            $maybe name <- maUser
+                <p>Hello #{name}, you're logged in.
+                $if name == adminName
+                    <form method=post>
+                        <button>Delete messages
+                        (Admin only)
                 <p>
                     <a href=@{AuthR LogoutR}>Logout
             $nothing
@@ -105,6 +110,11 @@ getHomeR = do
                     or register below.
                 ^{registerWidget widget enctype}
         |]
+
+postHomeR :: Handler ()
+postHomeR = do
+    setMessage "Done."
+    redirect HomeR
 
 registerWidget w e =
     [whamlet|
@@ -127,10 +137,20 @@ addUserToDB user = do
                         insert newUser >> return True
         Just _ -> return False
 
+isAdmin :: Handler AuthResult
+isAdmin = do
+    mu <- maybeAuth
+    return $ case mu of
+        Nothing -> AuthenticationRequired
+        Just x -> if (userName . entityVal) x == adminName
+                    then Authorized
+                    else Unauthorized "You must be an admin"
+
 main :: IO ()
 main = runNoLoggingT $ withSqliteConn "user.db3" $ \conn -> liftIO $ do
+    man <- newManager
     runSqlConn (runMigration migrateAll) conn
-    warp 3000 $ App conn
+    warp 3000 $ (App conn man)
 
 
 
